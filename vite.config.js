0a191e7b86588fc3858842ba4cd78c4b2d8fa25c
manh <manh99294@donga.edu.vn>
2025-04-21 21:45:51 +0700

    Allow Render domain in preview.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['web-twsl.onrender.com'] 
   }
 })
